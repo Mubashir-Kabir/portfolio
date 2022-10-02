b6c06d0ef462017ba36131d243cf497ed58e4a10
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-03 01:44:10 +0600

    active link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./Pages/About/About";
 import Blog from "./Pages/Blog/Blog";
 import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
         <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
         <Route path="/contact" element={<Contact></Contact>}></Route>
         <Route path="/blog" element={<Blog></Blog>}></Route>
-        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
--- src/Componants/Navbar/Navbar.js
+++ src/Componants/Navbar/Navbar.js
@@ -14,32 +14,57 @@ const Navbar = () => {
   return (
     <div className="flex bg-gray-900 text-white justify-center py-6 gap-20">
       <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
-        <NavLink to="/home">
+        <NavLink
+          to="/home "
+          className={({ isActive }) =>
+            isActive ? "text-orange-500" : undefined
+          }
+        >
           {" "}
           <FontAwesomeIcon icon={faHome} className="mr-2 " /> Home
         </NavLink>
       </div>
       <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
-        <NavLink to="/about">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500" : undefined
+          }
+        >
           {" "}
           <FontAwesomeIcon icon={faUser} className="mr-2 " />
           About
         </NavLink>
       </div>
       <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
-        <NavLink to="/portfolio">
+        <NavLink
+          to="/portfolio"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500" : undefined
+          }
+        >
           <FontAwesomeIcon icon={faBriefcase} className="mr-2 " />
           Portfolio
         </NavLink>
       </div>
       <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
-        <NavLink to="/contact">
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500" : undefined
+          }
+        >
           <FontAwesomeIcon icon={faEnvelopeOpen} className="mr-2 " />
           Contact
         </NavLink>
       </div>
       <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
-        <NavLink to="/blog">
+        <NavLink
+          to="/blog"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500" : undefined
+          }
+        >
           <FontAwesomeIcon icon={faBlog} className="mr-2 " />
           Blog
         </NavLink>
