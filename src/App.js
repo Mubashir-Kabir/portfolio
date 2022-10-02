0ec9e7422ffb9da672063fb29900412d228cd8e8
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-03 01:29:59 +0600

    simple navbar added

--- src/App.js
+++ src/App.js
@@ -10,18 +10,16 @@ import Portfolio from "./Pages/Portfolio/Portfolio";
 function App() {
   return (
     <div className="App">
-      <div className="">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/home" element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
-          <Route path="/contact" element={<Contact></Contact>}></Route>
-          <Route path="/blog" element={<Blog></Blog>}></Route>
-          <Route path="/" element={<Home></Home>}></Route>
-        </Routes>
-      </div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Componants/Navbar/Navbar.js
+++ src/Componants/Navbar/Navbar.js
@@ -1,14 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {
+  faHome,
+  faUser,
+  faBriefcase,
+  faEnvelopeOpen,
+  faBlog,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/home">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/Portfolio">Portfolio</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/blog">Blog</Link>
+    <div className="flex bg-gray-900 text-white justify-center py-6 gap-20">
+      <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
+        <NavLink to="/home">
+          {" "}
+          <FontAwesomeIcon icon={faHome} className="mr-2 " /> Home
+        </NavLink>
+      </div>
+      <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
+        <NavLink to="/about">
+          {" "}
+          <FontAwesomeIcon icon={faUser} className="mr-2 " />
+          About
+        </NavLink>
+      </div>
+      <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
+        <NavLink to="/portfolio">
+          <FontAwesomeIcon icon={faBriefcase} className="mr-2 " />
+          Portfolio
+        </NavLink>
+      </div>
+      <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
+        <NavLink to="/contact">
+          <FontAwesomeIcon icon={faEnvelopeOpen} className="mr-2 " />
+          Contact
+        </NavLink>
+      </div>
+      <div className="px-4 py-2 font-bold text-xl hover:text-orange-500">
+        <NavLink to="/blog">
+          <FontAwesomeIcon icon={faBlog} className="mr-2 " />
+          Blog
+        </NavLink>
+      </div>
     </div>
   );
 };
